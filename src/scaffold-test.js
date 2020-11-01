6adfbc84359e9c02814e6f61c0690318857241d4
Matt Travi <programmer@travi.org>
2020-11-01 00:01:28 -0500

    feat(next-steps): defined some initial steps

--- src/scaffold-test.js
+++ src/scaffold-test.js
@@ -3,6 +3,24 @@ import scaffold from './scaffold';
 
 suite('scaffold', () => {
   test('that the action is scaffolded', async () => {
-    assert.deepEqual(await scaffold(), {dependencies: ['@actions/core']});
+    const actionsDocumentationLink = 'https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/'
+      + 'creating-a-javascript-action';
+
+    assert.deepEqual(
+      await scaffold(),
+      {
+        dependencies: ['@actions/core'],
+        nextSteps: [
+          {
+            summary: 'Describe how to use the action in the README',
+            description: `[suggestions from the actions docs](${actionsDocumentationLink}#creating-a-readme)`
+          },
+          {
+            summary: 'Create the action metadata file',
+            description: `[details from the actions docs](${actionsDocumentationLink}#creating-an-action-metadata-file)`
+          }
+        ]
+      }
+    );
   });
 });
--- src/scaffold.js
+++ src/scaffold.js
@@ -1,3 +1,18 @@
 export default function () {
-  return {dependencies: ['@actions/core']};
+  const actionsDocumentationLink = 'https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/'
+    + 'creating-a-javascript-action';
+
+  return {
+    dependencies: ['@actions/core'],
+    nextSteps: [
+      {
+        summary: 'Describe how to use the action in the README',
+        description: `[suggestions from the actions docs](${actionsDocumentationLink}#creating-a-readme)`
+      },
+      {
+        summary: 'Create the action metadata file',
+        description: `[details from the actions docs](${actionsDocumentationLink}#creating-an-action-metadata-file)`
+      }
+    ]
+  };
 }
