a6924e2a4beb260efd2d083fc6a0e3bcd39f72ad
Matt Travi <programmer@travi.org>
2020-11-01 00:14:33 -0500

    feat(ncc): defined the build process to compiling the action into a single file
    based on
    https://docs.github.com/en/free-pro-team@latest/actions/creating-actions/creating-a-javascript-action#commit-tag-and-push-your-action-to-github

--- src/scaffold-test.js
+++ src/scaffold-test.js
@@ -10,6 +10,8 @@ suite('scaffold', () => {
       await scaffold(),
       {
         dependencies: ['@actions/core'],
+        devDependencies: ['@vercel/ncc'],
+        scripts: {build: 'ncc build index.js --license licenses.txt'},
         nextSteps: [
           {
             summary: 'Describe how to use the action in the README',
@@ -17,7 +19,10 @@ suite('scaffold', () => {
           },
           {
             summary: 'Create the action metadata file',
-            description: `[details from the actions docs](${actionsDocumentationLink}#creating-an-action-metadata-file)`
+            description: `[details from the actions docs](${actionsDocumentationLink}#creating-an-action-metadata-file)
+
+be sure to change the \`main\` keyword in your \`action.yml\` file to use the new \`dist/index.js\` file.
+\`main: 'dist/index.js'\``
           }
         ]
       }
--- src/scaffold.js
+++ src/scaffold.js
@@ -4,6 +4,8 @@ export default function () {
 
   return {
     dependencies: ['@actions/core'],
+    devDependencies: ['@vercel/ncc'],
+    scripts: {build: 'ncc build index.js --license licenses.txt'},
     nextSteps: [
       {
         summary: 'Describe how to use the action in the README',
@@ -11,7 +13,10 @@ export default function () {
       },
       {
         summary: 'Create the action metadata file',
-        description: `[details from the actions docs](${actionsDocumentationLink}#creating-an-action-metadata-file)`
+        description: `[details from the actions docs](${actionsDocumentationLink}#creating-an-action-metadata-file)
+
+be sure to change the \`main\` keyword in your \`action.yml\` file to use the new \`dist/index.js\` file.
+\`main: 'dist/index.js'\``
       }
     ]
   };
